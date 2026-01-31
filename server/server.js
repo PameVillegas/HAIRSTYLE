0345f0b1b23e0ea8d297b776e39e6726646dcb8e
PameVillegas <pamevillegas2022@gmail.com>
2026-01-31 19:22:13 -0300

    Mover ruta principal a routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { initializeDatabase } from './database.js';
+import systemRoutes from './routes/system.routes.js';
+
 
 dotenv.config();
 
@@ -15,16 +17,16 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use('/', systemRoutes);
+
 
 // Servir frontend en producción (opcional)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(join(__dirname, '../client/dist')));
 }
 
-// Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('✅ Servidor funcionando y base de datos lista!');
-});
+
+
 
 // Inicializar servidor
 async function startServer(port = 3000) {
