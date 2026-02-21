1951fe347864f3af00677f28212b3e90e1cc472b
PameVillegas <pamevillegas2022@gmail.com>
2026-02-21 13:40:03 -0300

    fix database file

--- server/database.js
+++ server/database.js
@@ -1,11 +1,11 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const connectionString = process.env.DATABASE_URL;
-
 export const pool = new Pool({
-  connectionString,
-  ssl: { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export async function initializeDatabase() {
